a425ad77cf5fc07a1ce9ca3312559fec17c7ee60
Fran Asensi <info@fjasensi.es>
2015-03-26 19:56:45 +0100

    Initial Commit

---
+++ Ejemplo llamada webserviceTests/Ejemplo_llamada_webserviceTests.swift
@@ -0,0 +1,36 @@
+//
+//  Ejemplo_llamada_webserviceTests.swift
+//  Ejemplo llamada webserviceTests
+//
+//  Created by Francisco Asensi Benito on 26/3/15.
+//  Copyright (c) 2015 Francisco Asensi Benito. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Ejemplo_llamada_webserviceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
